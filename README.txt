a2269881323ae27e81a8124e9f1373321288d96c
bihor <bihor@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-22 21:09:17 +0000

    First upload.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/myquizpoll/trunk@70234 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ README.txt
@@ -0,0 +1,3 @@
+Thank you for installing "My quiz and poll".
+You can use it as a quiz or poll.
+For more informations see "doc/manual.sxw".
---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "myquizpoll".
+#
+# Auto generated 11-09-2012 22:26
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'My quiz and poll',
+	'description' => 'An advanced quiz with a lot of features (e.g. highscore list, multi-page, multi-lingual, jokers) which can be used as a quiz, questionnaire, survey, poll, rating or psychological test',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '2.0.4',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => 'mod1',
+	'state' => 'stable',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Kurt Gusbeth',
+	'author_email' => 'info@myquizandpoll.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:87:{s:9:"ChangeLog";s:4:"6e79";s:10:"README.txt";s:4:"ab68";s:20:"class.ext_update.php";s:4:"44a1";s:12:"ext_icon.gif";s:4:"6763";s:30:"ext_icon_myquizpoll_folder.gif";s:4:"89e3";s:17:"ext_localconf.php";s:4:"da86";s:14:"ext_tables.php";s:4:"fa86";s:14:"ext_tables.sql";s:4:"ce4f";s:12:"flexform.xml";s:4:"24c1";s:31:"icon_tx_myquizpoll_category.gif";s:4:"78eb";s:31:"icon_tx_myquizpoll_question.gif";s:4:"8ad8";s:31:"icon_tx_myquizpoll_relation.gif";s:4:"401f";s:29:"icon_tx_myquizpoll_result.gif";s:4:"732d";s:29:"icon_tx_myquizpoll_voting.gif";s:4:"2c8c";s:13:"locallang.xml";s:4:"33d0";s:16:"locallang_db.xml";s:4:"eec3";s:16:"locallang_ff.xml";s:4:"f84f";s:7:"tca.php";s:4:"e2a7";s:14:"doc/manual.sxw";s:4:"4333";s:19:"doc/wizard_form.dat";s:4:"273a";s:20:"doc/wizard_form.html";s:4:"f2b0";s:23:"examples/antwort_bl.jpg";s:4:"be48";s:23:"examples/antwort_or.jpg";s:4:"bcb6";s:18:"examples/frage.jpg";s:4:"ac9e";s:21:"examples/joker_50.jpg";s:4:"2184";s:27:"examples/joker_audience.jpg";s:4:"d357";s:24:"examples/joker_phone.jpg";s:4:"c711";s:22:"examples/quiz_mill.css";s:4:"03c1";s:26:"examples/right_buttons.css";s:4:"347b";s:25:"examples/score_andere.jpg";s:4:"af0d";s:22:"examples/score_ich.jpg";s:4:"822b";s:30:"examples/template_address.html";s:4:"27a4";s:31:"examples/template_analysis.html";s:4:"2235";s:28:"examples/template_basic.html";s:4:"b059";s:33:"examples/template_fahrschule.html";s:4:"9b65";s:32:"examples/template_highscore.html";s:4:"34bf";s:33:"examples/template_millionaer.html";s:4:"38ed";s:27:"examples/template_poll.html";s:4:"856f";s:36:"examples/template_poll_advanced.html";s:4:"fa18";s:33:"examples/template_poll_basic.html";s:4:"0900";s:36:"examples/template_poll_enhanced.html";s:4:"6b2a";s:34:"examples/template_poll_jquery.html";s:4:"bb2a";s:37:"examples/template_poll_multipage.html";s:4:"a24f";s:29:"examples/template_psycho.html";s:4:"9f5c";s:34:"examples/template_quiz_jquery.html";s:4:"c123";s:35:"examples/template_quiz_jquery2.html";s:4:"6c2d";s:36:"examples/template_rating_jquery.html";s:4:"70de";s:36:"examples/template_rating_stars1.html";s:4:"ec70";s:36:"examples/template_rating_stars2.html";s:4:"b978";s:33:"examples/template_statistics.html";s:4:"44ad";s:20:"examples/zeit_bg.jpg";s:4:"dd99";s:13:"mod1/conf.php";s:4:"44f5";s:18:"mod1/icon-edit.jpg";s:4:"bd63";s:19:"mod1/icon-show1.jpg";s:4:"2746";s:19:"mod1/icon-show2.jpg";s:4:"cbc8";s:14:"mod1/index.php";s:4:"c534";s:18:"mod1/locallang.xml";s:4:"a8e2";s:22:"mod1/locallang_mod.xml";s:4:"942f";s:19:"mod1/moduleicon.gif";s:4:"b55b";s:18:"mod1/percent-l.jpg";s:4:"932e";s:18:"mod1/percent-r.jpg";s:4:"0dee";s:14:"mod1/style.css";s:4:"7c6b";s:14:"pi1/ce_wiz.gif";s:4:"7c02";s:41:"pi1/class.tx_myquizpoll_eidstarrating.php";s:4:"ae7e";s:34:"pi1/class.tx_myquizpoll_helper.php";s:4:"48c1";s:31:"pi1/class.tx_myquizpoll_pi1.php";s:4:"ecfb";s:39:"pi1/class.tx_myquizpoll_pi1_wizicon.php";s:4:"7bcc";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"4f5f";s:16:"pi1/poll_eID.php";s:4:"a145";s:16:"pi1/quiz_eID.php";s:4:"5a89";s:26:"pi1/tx_myquizpoll_pi1.tmpl";s:4:"105e";s:20:"pi1/static/dummy.css";s:4:"e869";s:24:"pi1/static/editorcfg.txt";s:4:"f949";s:20:"pi1/static/setup.txt";s:4:"8ce5";s:31:"res/css/jquery.ui.stars.min.css";s:4:"a705";s:22:"res/css/starrating.css";s:4:"5007";s:18:"res/img/delete.gif";s:4:"43e1";s:27:"res/img/jquery.ui.stars.gif";s:4:"a5b7";s:16:"res/img/star.gif";s:4:"3b7a";s:25:"res/js/jquery.MetaData.js";s:4:"1868";s:23:"res/js/jquery.rating.js";s:4:"9999";s:29:"res/js/jquery.ui.stars.min.js";s:4:"a5f4";s:36:"static/defaultsettings/constants.txt";s:4:"d41d";s:32:"static/defaultsettings/setup.txt";s:4:"bbcc";s:27:"static/starrating/setup.txt";s:4:"039f";s:24:"static/uistars/setup.txt";s:4:"a22a";}',
+	'suggests' => array(
+	),
+);
+
+?>
